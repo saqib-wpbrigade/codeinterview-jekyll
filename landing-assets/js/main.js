e10003427a55c6efaa07f772f52ef6d9f8fed6ac
Saqib-wpbrigade <saqib.wpbrigade@gmail.com>
2022-06-03 14:14:17 +0500

    forms code added

--- landing-assets/js/main.js
+++ landing-assets/js/main.js
@@ -132,3 +132,13 @@ if ($(".qoutes-section").length) {
 
 
 });
+
+const formkeepEmbed = document.querySelector('#formkeep-embed')
+
+formkeepEmbed.addEventListener('formkeep-embed:submitting', _event => {
+  console.log('Submitting form...')
+})
+
+formkeepEmbed.addEventListener('formkeep-embed:submitted', _event => {
+  console.log('Submitted form...')
+})
